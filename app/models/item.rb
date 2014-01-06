634420685c2a7c2f5fae707884b0a3a96e338b78
Vasily Kraev <mail@vkraev.ru>
2014-01-06 15:21:43 +0400

    lesson 6 - callbacks

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ActiveRecord::Base
   validates :price, numericality: { greater_than: 0, allow_nil: true }
   validates :name, :description, presence: true
+
+  belongs_to :category
+
+  after_initialize { p "initialized" }
+  after_save       { p "saved" }
+  after_create     { p "created"; } # ItemMailer.new_item_created(self).deliver # category.inc(:items_count, 1) }
+  after_update     { p "updated" }
+  after_destroy    { p "destroyed" } # category.inc(:items_count, -1) }
 end
