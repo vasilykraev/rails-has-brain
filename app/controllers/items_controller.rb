1ff3709e0071c4a2dfa55b4f70934e8d00575fb3
Vasily Kraev <mail@vkraev.ru>
2014-01-08 07:52:26 +0400

    lesson 8, part 1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,10 @@ class ItemsController < ApplicationController
     render text: @items.map { |i| "#{i.name}: #{i.price}"}.join("<br>")
   end
 
+  def create
+    @item = Item.create(name: params[:name], description: params[:name], weight: params[:weight], price: params[:price], real: params[:real])
+    render text: "#{@item.id}: #{@item.name} (#{!@item.new_record?})"
+  end
+
 end
+  
