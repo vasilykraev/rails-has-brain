0607e4330e5733f7f95b08adc2b0ca7b7f8adbd6
Vasily Kraev <mail@vkraev.ru>
2014-01-10 13:35:51 +0400

    lesson 9 - RESTful controllers, part 1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,10 +5,31 @@ class ItemsController < ApplicationController
     render text: @items.map { |i| "#{i.name}: #{i.price}"}.join("<br>")
   end
 
+  # /items/1 GET
+  def show
+  end
+
+  # /items/new GET
+  def new
+  end
+
+  # /items/1/edit GET
+  def edit
+  end
+
+  # /items POST
   def create
     item_params = params.require(:item).permit(:name, :description, :weight, :price, :real)
     @item = Item.create(item_params)
     render text: "#{@item.id}: #{@item.name} (#{!@item.new_record?})"
+  end
+
+  # /items/1 PUT + workaround POST
+  def update
+  end
+
+  # /items/1 DELETE + workaround POST
+  def destroy
   end
 
 end
